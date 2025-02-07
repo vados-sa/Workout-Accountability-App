eafabf6ed1531d2f2146c3c48b65ff7e05eb58e6
Vanessa dos Santos <smilzvanessa@gmail.com>
2025-02-07 19:53:09 -0300

    trying to deploy

--- workout-tracker/src/components/navbar.tsx
+++ workout-tracker/src/components/navbar.tsx
@@ -1,4 +1,18 @@
-export default function Navbar()
-{
-	return <nav>🏋️ Workout Tracker</nav>
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+
+export default function Navbar() {
+  const { data: session } = useSession();
+
+  return (
+    <nav>
+      <h2>🏋️ Workout Tracker</h2>
+      {session ? (
+        <button onClick={() => signOut()}>Logout</button>
+      ) : (
+        <a href="/login">Login</a>
+      )}
+    </nav>
+  );
 }
